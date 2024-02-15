df514e1f3bcd5f4da629e793e91aae571b59633c
Diego Suarez <suarezdiego297@gmail.com>
2024-02-15 14:16:40 -0500

    Agregar archivos nuevos al repositorio

---
+++ Desktop/Diego-developed/Landing-page/001-Agencia-Viajes/Js/ruta.js
@@ -0,0 +1,50 @@
+/**
+Variables e interface de Rutas 
+ */
+
+const flightPath = {
+  curviness: 1.5,
+  autoRotate: true,
+  values: [
+    {x: (window.innerWidth/8), y: -20},
+    {x: (window.innerWidth/8)*2, y: 10},
+    {x: (window.innerWidth/8)*3, y: 80},
+    {x: (window.innerWidth/8)*4, y: -60},
+    {x: (window.innerWidth/8)*3, y: -30},
+    {x: (window.innerWidth/8)*5, y: 70},
+    {x: (window.innerWidth/8)*7, y: 0},
+    {x: window.innerWidth + 150, y: 100},
+    {x: (window.innerWidth/8)*7, y: 0},
+    {x: (window.innerWidth/8)*5, y: 70},
+    {x: (window.innerWidth/8)*4, y: 80},
+    {x: (window.innerWidth/8)*3, y: 80},
+    {x: (window.innerWidth/8), y: -20},
+    {x: (window.innerWidth/8)*2, y: 10},
+    {x: (window.innerWidth/8)*7, y: 0},
+    {x: window.innerWidth + 150, y: 100}
+  ]
+}
+
+  
+  const tween = new TimelineLite()
+  
+  tween.add(
+    TweenLite.to('.paper-plane' , 3, {
+      bezier: flightPath,
+      ease: Power1.easeInOut
+    })
+  )
+  
+  const controller = new ScrollMagic.Controller()
+  const scene = new ScrollMagic.Scene({
+    triggerElement: '.animation',
+    duration: 1500,
+    triggerHook: 0
+  })
+  
+  .setTween(tween)
+  .addIndicators()
+  .setPin('.animation')
+  .addTo(controller)
+  
+  console.log('Paper plane image from pngtree.com')
